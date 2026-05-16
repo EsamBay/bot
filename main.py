b38c5117ae5d6450a5f26e82538613ddd01472c2
EsamBay <146492112+EsamBay@users.noreply.github.com>
2026-05-16 21:01:38 +0200

    Initialize bot with asyncio and aiogram

---
+++ main.py
@@ -0,0 +1,14 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from bot.config.config import BOT_TOKEN
+from bot.handlers.handlers import router
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    dp.include_router(router)
+    print("✅ Bot is running...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
